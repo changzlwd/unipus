bcd51cfe327eda6c52529f46e99fd6989b60fba9
changzlwd <changzlwd@users.noreply.github.com>
2026-06-12 01:02:52 +0000

    feat: 你好
    Co-authored-by: traeagent <traeagent@users.noreply.github.com>

--- consumerir.c
+++ consumerir.c
@@ -34,7 +34,6 @@
 #define ARRAY_SIZE(a) (sizeof(a) / sizeof((a)[0]))
 // liuqizhi 20260521 implement infrared remote control
 #define LIRC_DEVICE_PATH "/dev/lirc0"
-#define TRAILING_SPACE_US 10
 
 #ifndef TEMP_FAILURE_RETRY
 #define TEMP_FAILURE_RETRY(exp) ({ \
@@ -66,32 +65,20 @@ static int consumerir_transmit(struct consumerir_device *dev __unused,
 
     int final_len = pattern_len;
     const int *final_pattern = pattern;
-    unsigned int *tx_buf = NULL;
-    bool added_trailing = false;
 
     if (final_len % 2 == 0) {
-        final_len++;
-        tx_buf = malloc(final_len * sizeof(unsigned int));
-        if (!tx_buf) {
-            ALOGE("Failed to allocate memory for tx_buf");
-            return -1;
-        }
-        for (i = 0; i < pattern_len; i++) {
-            tx_buf[i] = (unsigned int)pattern[i];
-        }
-        tx_buf[final_len - 1] = TRAILING_SPACE_US;
-        added_trailing = true;
-        ALOGI("Pattern is even (%d), adding trailing space %d us, new length: %d",
-              pattern_len, TRAILING_SPACE_US, final_len);
-    } else {
-        tx_buf = malloc(final_len * sizeof(unsigned int));
-        if (!tx_buf) {
-            ALOGE("Failed to allocate tx buffer");
-            return -1;
-        }
-        for (i = 0; i < final_len; i++) {
-            tx_buf[i] = (unsigned int)final_pattern[i];
-        }
+        final_len--;
+        ALOGI("Pattern is even (%d), remove last slice, new length: %d",
+              pattern_len, final_len);
+    }
+
+    unsigned int *tx_buf = malloc(final_len * sizeof(unsigned int));
+    if (!tx_buf) {
+        ALOGE("Failed to allocate tx buffer");
+        return -1;
+    }
+    for (i = 0; i < final_len; i++) {
+        tx_buf[i] = (unsigned int)final_pattern[i];
     }
 
     fd = TEMP_FAILURE_RETRY(open(LIRC_DEVICE_PATH, O_RDWR));
